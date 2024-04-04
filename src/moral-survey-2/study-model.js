36d04b668e1c2273cddb01dfa39174cb529b8e75
Xinlong Wu <821408745@qq.com>
2024-04-04 23:53:11 +1100

    survey 3

--- src/moral-survey-2/study-model.js
+++ src/moral-survey-2/study-model.js
@@ -30,9 +30,7 @@ var mock_survey = require("./content/mocksurvey.html");
 var real_survey1 = require("./content/realsurvey1.html");
 var situation1 = require("./content/situation1.html");
 var situation2_ind = require("./content/situation2_ind.html");
-// var situation2_group = require("./content/situation2_group.html");
-// var situation3_ind = require("./content/situation3_ind.html");
-// var situation3_group = require("./content/situation3_group.html");
+var situation3_ind = require("./content/situation3_ind.html");
 // var comment = require("./content/comment.html");
 module.exports = (function(exports) {
 	var timeline = [],
@@ -107,13 +105,13 @@ module.exports = (function(exports) {
 			// 	display_element: $("#situation2_group"),
 			// 	display_next_button: false,
 			// },
-			// SITUATION3_IND: {
-			// 	name: "situation3_ind",
-			// 	type: "display-slide",
-			// 	template: situation3_ind,
-			// 	display_element: $("#situation3_ind"),
-			// 	display_next_button: false,
-			// },
+			SITUATION3_IND: {
+				name: "situation3_ind",
+				type: "display-slide",
+				template: situation3_ind,
+				display_element: $("#situation3_ind"),
+				display_next_button: false,
+			},
 			// COMMENT: {
 			// 	name: "comment",
 			// 	type: "display-slide",
@@ -158,8 +156,8 @@ module.exports = (function(exports) {
 		// timeline.push(params.slides.SURVEY1);
 		// timeline.push(params.slides.MOCK_SURVEY);
 		// timeline.push(params.slides.REAL_SURVEY1);
-		timeline.push(params.slides.SITUATION2_IND);
-		// timeline.push(params.slides.SITUATION3_IND);
+		// timeline.push(params.slides.SITUATION2_IND);
+		timeline.push(params.slides.SITUATION3_IND);
 		// timeline.push(params.slides.COMMENT);
 		// timeline.push(params.slides.SITUATION3_GROUP);
 		timeline.push(params.slides.COMMENTS);
