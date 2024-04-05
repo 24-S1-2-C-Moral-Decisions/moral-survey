5b6c906ebe94f2538497d3ae2c422999db5615ab
Xinlong Wu <821408745@qq.com>
2024-04-05 17:28:26 +1100

    build

---
+++ src/build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+for dir in moral-survey-*; do
+    if [ -d "$dir" ]; then
+    echo "Building $dir"
+    cd $dir
+    echo "[$dir] Install npm package"
+    npm install
+    echo "[$dir] Build pages"
+    npm run build
+    # cd $dir && ./your-script.sh
+    cd -
+    fi
+done
